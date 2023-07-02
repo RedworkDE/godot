a8267691ab979108e3967c073a4ba4cd06cfa244
RedworkDE <10944644+RedworkDE@users.noreply.github.com>
2023-07-02 19:09:59 +0200

    save all shader

--- drivers/vulkan/rendering_device_vulkan.cpp
+++ drivers/vulkan/rendering_device_vulkan.cpp
@@ -9034,7 +9034,7 @@ void RenderingDeviceVulkan::_load_pipeline_cache() {
 
 void RenderingDeviceVulkan::_update_pipeline_cache(bool p_closing) {
 	size_t pso_blob_size = 0;
-	float save_interval = GLOBAL_GET("rendering/rendering_device/pipeline_cache/save_chunk_size_mb");
+	float save_interval = 0; // GLOBAL_GET("rendering/rendering_device/pipeline_cache/save_chunk_size_mb");
 	VkResult vr = vkGetPipelineCacheData(device, pipelines_cache.cache_object, &pso_blob_size, nullptr);
 	ERR_FAIL_COND(vr);
 	size_t difference = (pso_blob_size - pipelines_cache.current_size) / (1024 * 1024);
