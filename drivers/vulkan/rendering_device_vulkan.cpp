bedcdcc94239bc3b62e4b6448eaab99902839093
RedworkDE <10944644+RedworkDE@users.noreply.github.com>
2023-07-02 16:28:59 +0200

    wait for cache done

--- drivers/vulkan/rendering_device_vulkan.cpp
+++ drivers/vulkan/rendering_device_vulkan.cpp
@@ -9471,6 +9471,9 @@ void RenderingDeviceVulkan::finalize() {
 		vkDestroyCommandPool(device, frames[i].command_pool, nullptr);
 		vkDestroyQueryPool(device, frames[i].timestamp_pool, nullptr);
 	}
+	if (pipelines_cache_save_task != WorkerThreadPool::INVALID_TASK_ID && !WorkerThreadPool::get_singleton()->is_task_completed(pipelines_cache_save_task)) {
+		WorkerThreadPool::get_singleton()->wait_for_task_completion(pipelines_cache_save_task);
+	}
 	// _update_pipeline_cache(true);
 
 	vkDestroyPipelineCache(device, pipelines_cache.cache_object, nullptr);
